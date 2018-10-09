5b5024f098613aa7febcab1d72d90f13773cc8c5
Hayri Cicek <hayricicek@Hayris-MBP.lan>
2018-10-09 11:01:43 +0200

    first commit

---
+++ src/main/java/com/kodnito/restapi/rest/ApplicationConfig.java
@@ -0,0 +1,13 @@
+package com.kodnito.restapi.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author hayricicek <hayri@kodnito.com>
+ */
+@ApplicationPath("/api")
+public class ApplicationConfig extends Application {
+
+}
